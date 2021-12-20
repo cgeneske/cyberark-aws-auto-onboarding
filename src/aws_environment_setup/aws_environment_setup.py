209b75d1b22871a786283cf325b64f3775ebf43f
cgeneske <cgeneske@outlook.com>
2021-12-20 17:52:42 -0500

    Refactored cfnresponse ReturnData as dict

--- src/aws_environment_setup/aws_environment_setup.py
+++ src/aws_environment_setup/aws_environment_setup.py
@@ -29,7 +29,8 @@ def lambda_handler(event, context):
             delete_params = delete_params_from_param_store(params_to_del)
             if not delete_params:
                 return cfnresponse.send(event, context, cfnresponse.FAILED,
-                                        "Failed to delete one or more parameters from parameter store, see detailed error in logs", {},
+                                        {'Data': "Failed to delete one or more parameters from parameter store, "
+                                                 "see detailed error in logs"},
                                         physical_resource_id)
             delete_sessions_table()
             return cfnresponse.send(event, context, cfnresponse.SUCCESS, None, {}, physical_resource_id)
