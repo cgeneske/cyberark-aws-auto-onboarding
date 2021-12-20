d2e48a223585f2c1a9bd1f8ed4cdb5aa1f294a01
cgeneske <cgeneske@outlook.com>
2021-12-20 12:26:17 -0500

    Adds Conjur Integration
    Adjusted VaultEnvCreation CFT to introduce new Conjur integration
    parameters.
    
    AOB mode constraint (SSL verification disabled) extended for the Conjur
    integration parameters.
    
    Statically named the CyberArk-AOB-SafeHandlerLambdaRole to support
    static host definition within Conjur for authentication.
    
    Extended parameter lookup and StoreParameters class to include new
    Conjur parameters.
    
    Implemented new conjur_integration module which uses the IAM
    Authenticator.
    
    Adjusted all PVWA logon calls to be preceded by a check on the
    AOB_Vault_User_Source to determine if vault user data will be derived
    from SSM Parameter Store or real-time CyberArk Conjur lookup, and
    handle accordingly.
    
    Improved parameter deletion logic to make it more extensible and
    resilient, adding a new Conjur parameter to the list.  Now if a
    parameter doesn't exist, it will no longer cause an immediate
    cfnresponse but will instead continue onto the next possible
    parameter for deletion.  This also removes the dependency on
    get_aob_mode as Production mode parameters will be attempted
    and simply skipped if missing in the parameter store.

--- src/aws_environment_setup/aws_environment_setup.py
+++ src/aws_environment_setup/aws_environment_setup.py
@@ -6,6 +6,7 @@ import boto3
 import cfnresponse
 from log_mechanism import LogMechanism
 from pvwa_integration import PvwaIntegration
+from conjur_integration import ConjurIntegration
 from dynamo_lock import LockerClient
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -22,14 +23,15 @@ def lambda_handler(event, context):
         physical_resource_id = str(uuid.uuid4())
         if 'PhysicalResourceId' in event:
             physical_resource_id = event['PhysicalResourceId']
-        # only deleting the vault_pass from parameter store
+        # Deleting parameters from parameter store that were created by this Lambda (during custom resource Create)
         if event['RequestType'] == 'Delete':
-            aob_mode = get_aob_mode()
             logger.info('Delete request received')
-            delete_params = delete_password_from_param_store(aob_mode)
+            params_to_del = ["AOB_Vault_Pass", "AOB_mode", "AOB_PVWA_Verification_Key", "AOB_Conjur_Verification_Key"]
+            delete_params = delete_params_from_param_store(params_to_del)
             if not delete_params:
                 return cfnresponse.send(event, context, cfnresponse.FAILED,
-                                        "Failed to delete 'AOB_Vault_Pass' from parameter store, see detailed error in logs", {},
+                                        {'Message': "Failed to delete one or more parameters from parameter store, "
+                                            "see detailed error in logs"},
                                         physical_resource_id)
             delete_sessions_table()
             return cfnresponse.send(event, context, cfnresponse.SUCCESS, None, {}, physical_resource_id)
@@ -48,39 +50,97 @@ def lambda_handler(event, context):
             request_aws_region_name = event['ResourceProperties']['AWSRegionName']
             request_aws_account_id = event['ResourceProperties']['AWSAccountId']
             request_s3_bucket_name = event['ResourceProperties']['S3BucketName']
-            request_verification_key_name = event['ResourceProperties']['PVWAVerificationKeyFileName']
+            request_pvwa_verification_key_name = event['ResourceProperties']['PVWAVerificationKeyFileName']
             aob_mode = event['ResourceProperties']['Environment']
+            request_vault_user_source = event['ResourceProperties']['VaultUserSource']
+            request_conjur_hostname = event['ResourceProperties']['ConjurHostname']
+            request_conjur_verification_key_name = event['ResourceProperties']['ConjurVerificationKeyFileName']
+            request_conjur_account = event['ResourceProperties']['ConjurAccount']
+            request_conjur_authn_service_id = event['ResourceProperties']['ConjurAuthnServiceID']
+            request_conjur_authn_host_namespace = event['ResourceProperties']['ConjurAuthnHostNamespace']
+            request_conjur_vault_username_id = event['ResourceProperties']['ConjurVaultUsernameID']
+            request_conjur_vault_password_id = event['ResourceProperties']['ConjurVaultPasswordID']
 
-
-            logger.info('Adding AOB_Vault_Pass to parameter store', DEBUG_LEVEL_DEBUG)
-            is_password_saved = add_param_to_parameter_store(request_password, "AOB_Vault_Pass", "Vault Password")
-            if not is_password_saved:  # if password failed to be saved
-                return cfnresponse.send(event, context, cfnresponse.FAILED,
-                                        "Failed to create Vault user's password in Parameter Store", {}, physical_resource_id)
-            if request_s3_bucket_name == '' and request_verification_key_name != '':
-                raise Exception('Verification Key cannot be empty if S3 Bucket is provided')
-            elif request_s3_bucket_name != '' and request_verification_key_name == '':
+            if request_vault_user_source == "SSM Parameter Store":
+                if request_username != '' and request_password != '':
+                    # Not using Conjur, we'll add the Vault Password to the parameter store
+                    logger.info('Using SSM Parameter Store for Vault User Details')
+                    logger.info('Adding AOB_Vault_Pass to parameter store', DEBUG_LEVEL_DEBUG)
+                    is_password_saved = add_param_to_parameter_store(request_password, "AOB_Vault_Pass", "Vault Password")
+                    if not is_password_saved:  # if password failed to be saved
+                        return cfnresponse.send(event, context, cfnresponse.FAILED,
+                                                {'Message': "Failed to create Vault user's password in Parameter Store"},
+                                                physical_resource_id)
+                    vault_username = request_username
+                    vault_password = request_password
+                else:
+                    return cfnresponse.send(event, context, cfnresponse.FAILED,
+                                            {'Message': "Vault Username or Vault Password parameters have not been defined"},
+                                            physical_resource_id)
+            else:
+                if (request_conjur_account != '' and request_conjur_hostname != '' and request_conjur_authn_service_id != ''
+                        and request_conjur_vault_username_id != '' and request_conjur_vault_password_id != ''):                        
+                    # Using Conjur
+                    logger.info('Using Conjur for Vault User Details')
+                    if aob_mode == 'Production':
+                        if request_s3_bucket_name == '' and request_conjur_verification_key_name != '':
+                            raise Exception('S3 Bucket cannot be empty if Conjur Verification key is provided')
+                        elif request_s3_bucket_name != '' and request_conjur_verification_key_name == '':
+                            raise Exception('Conjur Verification key cannot be empty if S3 Bucket is provided')
+                        logger.info('Adding Conjur CA cert to parameter store', DEBUG_LEVEL_DEBUG)
+                        is_conjur_cert_saved = save_verification_key_to_param_store(request_s3_bucket_name,
+                                                                                    request_conjur_verification_key_name,
+                                                                                    "conjur",
+                                                                                    "AOB_Conjur_Verification_Key",
+                                                                                    "Conjur Verification Key")
+                        if not is_conjur_cert_saved:  # if Conjur verification key failed to be saved
+                            return cfnresponse.send(event, context, cfnresponse.FAILED,
+                                                    {'Message': "Failed to create Conjur Verification Key in Parameter Store"},
+                                                    physical_resource_id)
+                    
+                    account_id = context.invoked_function_arn.split(':')[4]
+                    if request_conjur_authn_host_namespace == 'root':
+                        conjur_username = f'host/{account_id}/CyberArk-AOB-SafeHandlerLambdaRole'
+                    else:
+                        conjur_username = f'host/{request_conjur_authn_host_namespace}/{account_id}/CyberArk-AOB-SafeHandlerLambdaRole'
+                    conjur_integration_class = ConjurIntegration(IS_SAFE_HANDLER, aob_mode)
+                    conjur_token = conjur_integration_class.logon_conjur(request_conjur_hostname, request_conjur_account, 
+                                                                         request_conjur_authn_service_id, conjur_username)
+                    vault_username = conjur_integration_class.get(conjur_token, request_conjur_vault_username_id)
+                    vault_password = conjur_integration_class.get(conjur_token, request_conjur_vault_password_id)
+                else:
+                    return cfnresponse.send(event, context, cfnresponse.FAILED,
+                                            {'Message': "One or more Conjur parameters have not been defined"},
+                                            physical_resource_id)
+      
+                           
+            if request_s3_bucket_name == '' and request_pvwa_verification_key_name != '':
                 raise Exception('S3 Bucket cannot be empty if Verification Key is provided')
+            elif request_s3_bucket_name != '' and request_pvwa_verification_key_name == '':
+                raise Exception('Verification Key cannot be empty if S3 Bucket is provided')
             else:
                 logger.info('Adding AOB_mode to parameter store', DEBUG_LEVEL_DEBUG)
                 is_aob_mode_saved = add_param_to_parameter_store(aob_mode, 'AOB_mode',
-                                                                 'Dictates if the solution will work in POC(no SSL) or ' \
-                                                                 'Production(with SSL) mode')
-                if not is_aob_mode_saved:  # if password failed to be saved
+                                                                 'Dictates if the solution will work in POC (no SSL) or ' \
+                                                                 'Production (with SSL) mode')
+                if not is_aob_mode_saved:  # if AOB_mode failed to be saved
                     return cfnresponse.send(event, context, cfnresponse.FAILED,
                                             "Failed to create AOB_mode parameter in Parameter Store", {}, physical_resource_id)
                 if aob_mode == 'Production':
                     logger.info('Adding verification key to Parameter Store', DEBUG_LEVEL_DEBUG)
                     is_verification_key_saved = save_verification_key_to_param_store(request_s3_bucket_name,
-                                                                                     request_verification_key_name)
-                    if not is_verification_key_saved:  # if password failed to be saved
+                                                                                     request_pvwa_verification_key_name,
+                                                                                     "pvwa",
+                                                                                     "AOB_PVWA_Verification_Key",
+                                                                                     "PVWA Verification Key")
+                    if not is_verification_key_saved:  # If PVWA Verification Key failed to be saved
                         return cfnresponse.send(event, context, cfnresponse.FAILED,
                                                 "Failed to create PVWA Verification Key in Parameter Store",
                                                 {}, physical_resource_id)
 
             pvwa_integration_class = PvwaIntegration(IS_SAFE_HANDLER, aob_mode)
             pvwa_url = f"https://{request_pvwa_ip}/PasswordVault"
-            pvwa_session_id = pvwa_integration_class.logon_pvwa(request_username, request_password, pvwa_url, "1")
+            pvwa_session_id = pvwa_integration_class.logon_pvwa(vault_username, vault_password, pvwa_url, "1")
             if not pvwa_session_id:
                 return cfnresponse.send(event, context, cfnresponse.FAILED,
                                         "Failed to connect to PVWA, see detailed error in logs", {}, physical_resource_id)
@@ -265,15 +325,15 @@ def create_session_table():
     return True
 
 
-def save_verification_key_to_param_store(s3_bucket_name, verification_key_name):
+def save_verification_key_to_param_store(s3_bucket_name, verification_key_name, verification_key_type, parameter_name, parameter_description):
     logger.trace(s3_bucket_name, verification_key_name, caller_name='save_verification_key_to_param_store')
     try:
-        logger.info('Downloading verification key from s3')
+        logger.info(f'Downloading {verification_key_name} from S3 bucket - {s3_bucket_name}')
         s3_resource = boto3.resource('s3')
-        s3_resource.Bucket(s3_bucket_name).download_file(verification_key_name, '/tmp/server.crt')
-        add_param_to_parameter_store(open('/tmp/server.crt').read(), "AOB_PVWA_Verification_Key", "PVWA Verification Key")
+        s3_resource.Bucket(s3_bucket_name).download_file(verification_key_name, f'/tmp/{verification_key_type}_server.crt')
+        add_param_to_parameter_store(open(f'/tmp/{verification_key_type}_server.crt').read(), parameter_name, parameter_description)
     except Exception as e:
-        logger.error(f"An error occurred while downloading Verification Key from S3 Bucket - {s3_bucket_name}. Exception: {e}")
+        logger.error(f"An error occurred while downloading {verification_key_name} from S3 Bucket - {s3_bucket_name}. Exception: {e}")
         return False
     return True
 
@@ -295,30 +355,22 @@ def add_param_to_parameter_store(value, parameter_name, parameter_description):
     return True
 
 
-def delete_password_from_param_store(aob_mode):
-    logger.trace(aob_mode, caller_name='delete_password_from_param_store')
-    try:
-        logger.info('Deleting parameters from parameter store')
-        ssm_client = boto3.client('ssm')
-        ssm_client.delete_parameter(
-            Name='AOB_Vault_Pass'
-        )
-        logger.info("Parameter 'AOB_Vault_Pass' deleted successfully from Parameter Store")
-        ssm_client.delete_parameter(
-            Name='AOB_mode'
-        )
-        logger.info("Parameter 'AOB_mode' deleted successfully from Parameter Store")
-        if aob_mode == 'Production':
-            ssm_client.delete_parameter(
-                Name='AOB_PVWA_Verification_Key'
-            )
-            logger.info("Parameter 'AOB_PVWA_Verification_Key' deleted successfully from Parameter Store")
-        return True
-    except Exception as e:
-        if e.response["Error"]["Code"] == "ParameterNotFound":
-            return True
-        logger.error(f'Failed to delete parameter "Vault_Pass" from Parameter Store. Error code: {e.response["Error"]["Code"]}')
-        return False
+def delete_params_from_param_store(params_to_del):
+    logger.trace(params_to_del, caller_name='delete_params_from_param_store')
+    logger.info('Deleting parameters from parameter store')
+    state = True
+    ssm_client = boto3.client('ssm')
+    for param in params_to_del:
+        try:
+            ssm_client.delete_parameter(Name=f'{param}')
+            logger.info(f"Parameter '{param}' deleted successfully from Parameter Store")
+        except Exception as e:
+            if e.response["Error"]["Code"] == "ParameterNotFound":
+                logger.info(f"Parameter '{param}' was not found, skipping")
+            else:
+                logger.error(f'Failed to delete parameter "{param}" from Parameter Store. Error code: {e.response["Error"]["Code"]}')
+                state = False   
+    return state
 
 
 def delete_sessions_table():
@@ -332,12 +384,3 @@ def delete_sessions_table():
     except Exception:
         logger.error("Failed to delete 'Sessions' table from DynamoDB")
         return
-
-
-def get_aob_mode():
-    ssm = boto3.client('ssm')
-    ssm_parameter = ssm.get_parameter(
-        Name='AOB_mode'
-    )
-    aob_mode = ssm_parameter['Parameter']['Value']
-    return aob_mode
---
+++ src/shared_libraries/conjur_integration.py
@@ -0,0 +1,82 @@
+import hashlib
+import json
+import urllib.parse
+import requests
+import aws_services
+from log_mechanism import LogMechanism
+import boto3
+from botocore.auth import SigV4Auth
+from botocore.awsrequest import AWSRequest
+
+DEBUG_LEVEL_DEBUG = 'debug'  # Outputs all information
+
+
+class ConjurIntegration:
+    def __init__(self, is_safe_handler=False, safe_handler_environment=None):
+        self.logger = LogMechanism()
+        self.logger.trace(is_safe_handler, safe_handler_environment, caller_name='__init__')
+        self.is_safe_handler = is_safe_handler
+        self.safe_handler_environment = safe_handler_environment
+        self.conjur_url = ''
+        self.conjur_account = ''
+        try:
+            self.logger.info('Conjur Integration - Getting parameters from parameter store')
+            if not is_safe_handler:
+                parameters = aws_services.get_params_from_param_store()
+                environment = parameters.aob_mode
+            else:
+                environment = self.safe_handler_environment
+            if environment == 'Production':
+                self.logger.info(f'{environment} Environment Detected', DEBUG_LEVEL_DEBUG)
+                self.certificate = "/tmp/conjur_server.crt"
+            else:
+                self.logger.info(f'{environment} Environment Detected', DEBUG_LEVEL_DEBUG)
+                self.certificate = False
+        except Exception as e:
+            self.logger.error(f'Failed to retrieve aob_mode parameter: {str(e)}')
+            raise Exception("Error occurred while retrieving aob_mode parameter")            
+
+    def logon_conjur(self, conjur_hostname, conjur_account, service_id, host_id):
+        self.logger.trace(conjur_hostname, conjur_account, service_id, host_id, caller_name='logon_conjur')
+        self.logger.info("Authenticating to Conjur")
+        self.conjur_url = f"https://{conjur_hostname}"
+        self.conjur_account = conjur_account
+
+        session = boto3.Session()
+        credentials = session.get_credentials()
+        creds = credentials.get_frozen_credentials()
+
+        self.logger.info("Generating signed request to STS for Conjur AWS IAM Authenticator", DEBUG_LEVEL_DEBUG)
+        request = AWSRequest("GET", "https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15")
+        payload_hash = hashlib.sha256(('').encode('utf-8')).hexdigest()
+        request.headers.add_header("Host", "sts.amazonaws.com")
+        request.headers.add_header("X-Amz-Content-sha256", payload_hash)
+        request.headers.add_header("Accept-Encoding", "identity")
+        request.headers.add_header("Content-Type", "application/x-www-form-urlencoded")
+        SigV4Auth(creds, "sts", "us-east-1").add_auth(request)
+        headers = {key: value for (key, value) in request.headers.items()}
+
+        host_id_safe = urllib.parse.quote(host_id, safe='')
+        auth_url = f"{self.conjur_url}/authn-iam/{service_id}/{conjur_account}/{host_id_safe}/authenticate"
+        
+        self.logger.info(f"Authenticating to Conjur Endpoint - {auth_url}")
+        auth_res = requests.post(auth_url, json.dumps(headers), headers={
+                'Accept-Encoding': "base64"
+            }, verify=self.certificate, timeout=5)
+        print(f"Get conjur token: {auth_res.status_code}:{auth_res.reason}")
+        if auth_res.status_code != 200:
+            raise Exception(f"Conjur authentication failure: {auth_res.status_code}:{auth_res.reason}")
+        return auth_res.text
+
+    def get(self, conjur_token, variable_id):
+        self.logger.trace(variable_id, caller_name='get')
+        self.logger.info("Retrieving Conjur Variable from Endpoint - "
+                         f"{self.conjur_url}/secrets/{self.conjur_account}/variable/{variable_id}", DEBUG_LEVEL_DEBUG)
+        res = requests.get(f"{self.conjur_url}/secrets/{self.conjur_account}/variable/{variable_id}", headers={
+                           'Authorization': f"Token token=\"{conjur_token}\""
+                           }, verify=self.certificate, timeout=5)
+            
+        if res.status_code != 200:
+            raise Exception(f"Failed to get secret {variable_id}. {res.status_code}:{res.reason}")
+
+        return res.text
